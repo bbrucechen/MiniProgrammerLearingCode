edfcdf95b5f5eeb30dc106c03043ee7ff58fcff8
brucechen <424091906@qq.com>
2020-01-28 12:20:08 +0800

    wxml的判断循环等

--- pages/wxml/wxml.js
+++ pages/wxml/wxml.js
@@ -7,7 +7,21 @@ Page({
   data: {
     message:'hello kobe',
     nowDate:new Date().toLocaleString(),
-    fontColor:'red'
+    fontColor:'red',
+    isShow:false,
+    score:97
+  },
+  inputDone(e) {
+    console.log(e)
+    let value = Number.parseInt(e.detail.value)
+    this.setData({
+      score:value
+    })
+  },
+  switch() {
+    this.setData({
+      isShow:!this.data.isShow
+    })
   },
   onLoad() {
     setInterval(() => {
