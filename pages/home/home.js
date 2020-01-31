08a2906e5f05e724957faf8951103f7da43f1d58
brucechen <424091906@qq.com>
2020-01-31 12:10:24 +0800

    小程序的弹窗/分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,60 +7,68 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  toastShow() {
+    wx.showToast({
+      title: 'i"am toast!',
+      duration:500,
+      icon:'loading',
+      mask:true,
+      success() {
+        console.log('success')
+      },
+      fail() {
+        console.log('fail')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  showModal() {
+    wx.showModal({
+      title: 'modal',
+      content: '傻逼',
+      // 一旦用户点击了确认或取消就触发success
+      success(res) {
+        console.log(res)
+        // 用户的应答信息都包含在success函数的参数中
+        if(res.cancel) {
+          console.log('用户取消')
+        }
+        if(res.confirm) {
+          console.log('用户确认')
+        }
+      },
+      cancelText:'退出'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  showLoading() {
+    wx.showLoading({
+      title: '稍等',
+      mask:true
+    })
+    setTimeout(() =>{
+      // wx的hideloading可以关闭loading弹窗
+      wx.hideLoading()
+    },3000)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  showAction() {
+    wx.showActionSheet({
+      itemList: [
+        '相册',
+        '拍照'
+      ],
+      itemColor:'red',
+      success:function(res) {
+        console.log(res)
+        // 通过res中的tapIndex可以得知用户选中哪个选项
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 必须配置onShareAppMessage函数才能使用分享功能
+  onShareAppMessage(op) {
+    // 返回一个对象
+    return {
+      title:'hello',
+      path:'/pages/about/about',
+      imageUrl:'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }
   }
 })
